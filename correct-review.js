2da448b8e69ae34076207c33c68ae6a6f8648d8a
Rory Hayes <roryohayes@gmail.com>
2016-09-11 22:27:15 -0700

    light refactor and packed extension

--- correct-review.js
+++ correct-review.js
@@ -12,17 +12,23 @@ var realAverage = ((percent5*5)+(percent4*4)+(percent3*3)+(percent2*2)+(percent1
 document.getElementsByClassName("arp-rating-out-of-text")[0].innerHTML = realAverage + " out of 5 stars"
 
 // replace the star icons, according the same rounding that Amazon already uses
-if (realAverage < 0.3) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-0 averageStarRating";
-else if (realAverage < 0.8) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-0-5 averageStarRating";
-else if (realAverage < 1.3) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-1 averageStarRating";
-else if (realAverage < 1.8) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-1-5 averageStarRating";
-else if (realAverage < 2.3) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-2 averageStarRating";
-else if (realAverage < 2.8) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-2-5 averageStarRating";
-else if (realAverage < 3.3) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-3 averageStarRating";
-else if (realAverage < 3.8) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-3-5 averageStarRating";
-else if (realAverage < 4.3) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-4 averageStarRating";
-else if (realAverage < 4.8) document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-4-5 averageStarRating";
-else document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium a-star-medium-5 averageStarRating";
+var starClassName = "a-star-medium-5";
+
+if (realAverage < 0.3) starClassName = "a-star-medium-0";
+else if (realAverage < 0.8) starClassName = "a-star-medium-0-5";
+else if (realAverage < 1.3) starClassName = "a-star-medium-1";
+else if (realAverage < 1.8) starClassName = "a-star-medium-1-5";
+else if (realAverage < 2.3) starClassName = "a-star-medium-2";
+else if (realAverage < 2.8) starClassName = "a-star-medium-2-5";
+else if (realAverage < 3.3) starClassName = "a-star-medium-3";
+else if (realAverage < 3.8) starClassName = "a-star-medium-3-5";
+else if (realAverage < 4.3) starClassName = "a-star-medium-4";
+else if (realAverage < 4.8) starClassName = "a-star-medium-4-5";
+
+document.getElementsByClassName("averageStarRating")[0].className = "a-icon a-icon-star-medium " + starClassName + " averageStarRating";
 
 // remember to test this on the product page AND the review page
 // remember we shouldn't break other product's reviews
+// update stars at top (and inside the popup)
+// update stars and text at bottom
+//document.getElementById("histogramTable").getElementsByClassName("a-link-normal")
